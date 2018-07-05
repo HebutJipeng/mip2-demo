067be54f1e354b014702818436ce0debc8ca10d2
jipeng <jipeng@gotokeep.com>
2018-07-05 12:26:00 +0800

    init demo

---
+++ components/mip-example/mip-example.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <h1>MIP 2.0 component example2</h1>
+    <h3>This is my first custom component !</h3>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      message: 'hello world!'
+    }
+  },
+  mounted () {
+    // axios.get('')
+    console.log('This is my first custom component !')
+  }
+}
+</script>
---
+++ components/mip-me/mip-me.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <h1>MIP ME</h1>
+    <h3>This is my first custom component !</h3>
+    <h2>authorizations_url: {{ url }}</h2>
+    <mip-img class="demo-img" layout="responsive" src="/static/2018050712241542.png">
+    </mip-img>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+.demo-img {
+  height: 100px;
+}
+</style>
+
+<script>
+import request from '../../utils/request'
+export default {
+  data () {
+    return {
+      url: ''
+    }
+  },
+  mounted () {
+    const self = this
+    request.get('').then(res => {
+      console.log(res)
+      self.url = res.data.authorizations_url
+    })
+    console.log('This is me')
+  }
+}
+</script>
