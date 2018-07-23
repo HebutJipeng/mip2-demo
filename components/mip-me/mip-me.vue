5883fd31107e0cfe11d0cbc57da90b28b8de4c95
HebutJipeng <282602893@qq.com>
2018-07-23 17:35:18 +0800

    选择

--- components/mip-me/mip-me.vue
+++ components/mip-me/mip-me.vue
@@ -5,6 +5,17 @@
     <h2>authorizations_url: {{ url }}</h2>
     <mip-img class="demo-img" layout="responsive" src="/static/2018050712241542.png">
     </mip-img>
+    <hr>
+    <br>
+    <div v-for="i in 5">
+      <button @click="add(i, i+'_my_value')" class="btn">add+{{i}}</button>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <hr>
+    <button @click="alert()">输出</button>
   </div>
 </template>
 
@@ -23,7 +34,8 @@ import request from '../../utils/request'
 export default {
   data () {
     return {
-      url: ''
+      url: '',
+      sum: []
     }
   },
   mounted () {
@@ -33,6 +45,36 @@ export default {
       self.url = res.data.authorizations_url
     })
     console.log('This is me')
+  },
+  methods: {
+    add(idx, myvalue) {
+      var tem = {
+        id: idx,
+        value: myvalue
+      }
+      let flag = true,
+          flag_id
+      this.sum.length !== 0 && this.sum.forEach((item, index) => {
+        if(item.value == myvalue) {
+          flag = false
+          flag_id = index
+        }
+      })
+      if(!flag) {
+        console.log('对象中已存在该值，现在删除')
+        this.sum.splice(flag_id, 1)
+      } else {
+        this.sum.push(tem)
+      }
+      console.log(this.sum)
+    },
+    alert() {
+      let a = this.sum.map((item, idx) =>{
+        return item.value
+      })
+      console.log(a.join('_'))
+      // 提交
+    }
   }
 }
 </script>
